c5c3c220a4c383f51c04c7884c3dda1327d6810b
cmur85 <39341418+cmurestudillos@users.noreply.github.com>
2020-09-29 14:00:51 +0200

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/app';
 import 'firebase/auth';
  
 const firebaseConfig  = {
-    apiKey: "AIzaSyBpuR-S22M0WnCo0-iQI94Fo5E1r7ALedc",
-    authDomain: "blogframeworks.firebaseapp.com",
-    databaseURL: "https://blogframeworks.firebaseio.com",
-    projectId: "blogframeworks",
-    storageBucket: "blogframeworks.appspot.com",
-    messagingSenderId: "291878683328",
-    appId: "1:291878683328:web:c4595fc170cfc731f6a325",
-    measurementId: "G-2W9Y94K8SB"
+    apiKey: "XXXXXXX",
+    authDomain: "XXXXXXX",
+    databaseURL: "XXXXXXX",
+    projectId: "XXXXXXX",
+    storageBucket: "XXXXXXX",
+    messagingSenderId: "XXXXXXX",
+    appId: "XXXXXXX",
+    measurementId: "XXXXXXX"
 };
 
 firebase.initializeApp(firebaseConfig);
