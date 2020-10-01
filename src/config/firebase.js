13acb63ac391ffeabc76fd72ec102a737929a1b1
carlos mur <cmurestudillos@gmail.com>
2020-10-01 09:14:25 +0200

    commit

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import 'firebase/app';
 import 'firebase/auth';
  
 const firebaseConfig  = {
-    apiKey: "XXXXXXX",
-    authDomain: "XXXXXXX",
-    databaseURL: "XXXXXXX",
-    projectId: "XXXXXXX",
-    storageBucket: "XXXXXXX",
-    messagingSenderId: "XXXXXXX",
-    appId: "XXXXXXX",
-    measurementId: "XXXXXXX"
+    apiKey: "AIzaSyBpuR-S22M0WnCo0-iQI94Fo5E1r7ALedc",
+    authDomain: process.env.VUE_APP_AUTH_DOMAIN,
+    databaseURL: process.env.VUE_APP_DATABASE_URL,
+    projectId: process.env.VUE_APP_PROJECT_ID,
+    storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VUE_APP_APP_ID,
+    measurementId: process.env.VUE_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
